e20d80488a5a45370d269da17eccb04d229de9b5
LeoFdz <lfernandez79@gmail.com>
2020-03-23 16:29:29 -0500

    updated Appjs file with Route base on previous activities

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import API from "./utils/API";
 import EmployeeCard from "./components/EmployeeCard";
 import Header from "./components/Header";
+import SortForm from "./components/SortForm";
 
+//employeeList gets filled and then doesn't change after that
 var employeeList;
 
-class App extends Component {
+function App() {
 
-  state = {
-    employees: []
-  };
+  const [employees, setEmployees] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     API.getUsers()
       .then((result) => employeeList = result.data.results)
-      .then(() => this.setState({ employees: employeeList }))
+      .then(() => setEmployees(employeeList))
       .catch(err => console.log(err));
+  }, []);
+
+  //filter by gender
+  const getGender = (gender) => {
+    return employees.filter((employee) => employee.gender === gender);
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.employees.map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
-          email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
-      </div>
-    );
+  //sort by alphabet
+  const alphabetize = (arr) => {
+    return arr.sort((emp1, emp2) => emp1.name.last.localeCompare(emp2.name.last));
   }
+
+  return (
+    <Router>
+      <div className="bg-dark">
+        <Header />
+        <div className="container">
+          <SortForm />
+
+          <Route path="/">
+            <div>
+              {employees.map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
+                email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
+            </div>
+          </Route>
+
+          <Route path="/employee-directory">
+            <div>
+              {employees.map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
+                email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
+            </div>
+          </Route>
+
+          <Route path="/male">
+            <div>
+              {getGender("male").map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
+                email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
+            </div>
+          </Route>
+
+          <Route path="/female">
+            <div>
+              {getGender("female").map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
+                email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
+            </div>
+          </Route>
+
+          <Route path="/sort">
+            <div>
+              {alphabetize(employees).map(employee => <EmployeeCard firstName={employee.name.first} lastName={employee.name.last}
+                email={employee.email} phone={employee.phone} src={employee.picture.medium} />)}
+            </div>
+          </Route>
+
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
